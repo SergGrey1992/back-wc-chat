95aed503d4c8169b927ce7e525254b60c9886500
SergGrey <sergdiag19@gmail.com>
2021-05-28 19:47:56 +0300

    first commit

---
+++ dist/app.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var http_1 = __importDefault(require("http"));
+var socket_io_1 = __importDefault(require("socket.io"));
+var app = express_1.default();
+var server = http_1.default.createServer(app);
+// @ts-ignore
+var socket = socket_io_1.default(server);
+var messages = [
+    { message: 'Hello Serg', id: 'qweqwe', user: { id: 'qweqew', name: 'Dimych' } },
+    { message: 'Hello Dima', id: 'qweqwe1', user: { id: 'qweqew1', name: 'Sergey' } },
+];
+socket.on('connecting', function (socketChannel) {
+    socketChannel.on('client-message-sent', function (message) {
+        var messageItem = { message: message, id: '123' + new Date(), user: { id: 'effeff', name: 'Sergey' } };
+        messages.push(messageItem);
+        socket.emit('new-message-sent', messageItem);
+    });
+    socketChannel.emit('init-messages-published', messages);
+    console.log('a user connected');
+});
+var PORT = process.env.PORT || 3009;
+server.listen(PORT, function () {
+    console.log('listening on *:3009');
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import http from 'http'
+import socketio from 'socket.io'
+
+const app = express();
+const server = http.createServer(app);
+// @ts-ignore
+const socket = socketio(server);
+const messages = [
+	{message: 'Hello Serg', id: 'qweqwe', user: {id: 'qweqew', name: 'Dimych'}},
+	{message: 'Hello Dima', id: 'qweqwe1', user: {id: 'qweqew1', name: 'Sergey'}},
+]
+socket.on('connecting', (socketChannel: any) => {
+	socketChannel.on('client-message-sent', (message: string) => {
+		let messageItem = {message, id: '123' + new Date(), user: {id: 'effeff', name: 'Sergey'}}
+		messages.push(messageItem)
+
+		socket.emit('new-message-sent', messageItem)
+	})
+
+	socketChannel.emit('init-messages-published', messages);
+	console.log('a user connected')
+})
+
+const PORT = process.env.PORT || 3009;
+
+server.listen(PORT, () => {
+	console.log('listening on *:3009')
+})
